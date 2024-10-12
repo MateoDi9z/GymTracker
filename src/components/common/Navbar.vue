b7264e8afbf8c32a541ca3b675bc7446f4ceccf4
MateoDi9z <dzmateozd@gmail.com>
2024-10-12 20:36:18 -0300

    navbar done

---
+++ src/components/common/Navbar.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+const links = [
+    {
+        name: "Home",
+        to: "/"
+    },
+    {
+        name: "Agregar Serie",
+        to: "/addSerie"
+    },
+    {
+        name: "About",
+        to: "/about"
+    },
+]
+</script>
+
+<template>
+    <header class="navbar bg-violet-800 text-white">
+
+        <div class="navbar-start">
+            <div class="dropdown lg:hidden">
+                <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h7" />
+                    </svg>
+                </div>
+                <ul tabindex="0"
+                    class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                    <li v-for="link in links">
+                        <RouterLink :to="link.to">{{ link.name }}</RouterLink>
+                    </li>
+                </ul>
+            </div>
+
+
+            <div class="hidden lg:flex">
+                <ul class="menu menu-horizontal px-1">
+                    <li>
+                        <RouterLink class="rounded-xl font-bold" to="/">Home
+                        </RouterLink>
+                    </li>
+                    <!--<li>
+
+                            <details>
+                                <summary>Series</summary>
+                                <ul class="p-2">
+                                    <li><a>Ver</a></li>
+                                    <li><a>Agregar</a></li>
+                                </ul>
+                            </details>
+                </li>-->
+                    <li>
+                        <RouterLink class="rounded-xl font-bold" to="/about">About</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink class="rounded-xl font-bold" to="/addSerie">Add
+                            Serie</RouterLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="navbar-center">
+            <RouterLink to="/">
+                <button class="btn btn-ghost text-xl text-white" style="text-decoration: none !important;">Gym Track
+                    App</button>
+            </RouterLink>
+        </div>
+
+        <div class="navbar-end">
+            <!-- <a class="btn">Refresh</a> -->
+        </div>
+    </header>
+</template>
+
+<style scoped>
+header .menu a.router-link-active {
+    color: #fff;
+    opacity: .8;
+    text-decoration-line: underline;
+    text-decoration-thickness: auto;
+}
+
+header a.router-link-exact-active:hover {
+    background-color: transparent;
+}
+
+header a:first-of-type {
+    border: 0;
+}
+</style>
--- src/views/CreateSerie.vue
+++ src/views/CreateSerie.vue
@@ -10,50 +10,48 @@ const data = reactive({
     weight: null,
 })
 
-    const store = useDataManager()
+const store = useDataManager()
 
-    if (!store.getExercises.length) store.fetchExercises()
+if (!store.getExercises.length) store.fetchExercises()
 </script>
 
 <template>
-    <div class="box">
-        <div class="wrapper flex flex-col gap-4">
-            <h1 class="text-xl pt-5 text-primary tracking-wide">Registrar Serie:</h1>
-            <hr class="border-secondary" />
+    <div class="wrapper flex flex-col gap-4 px-5 box">
+        <h1 class="text-xl pt-5 text-primary tracking-wide">Registrar Serie:</h1>
+        <hr class="border-secondary" />
 
-            <label class="form-control w-full max-w-xs">
-                <div class="label">
-                    <span class="label-text">Tipo de ejercicio</span>
-                    <span class="label-text-alt">Crear uno</span>
-                </div>
-                <select class="select select-bordered capitalize" >
-                    <option disabled selected> 
-                        Seleccionar un ejercicio
-                    </option>
-                    <option v-for="ex in store.getExercises">
-                        {{ ex.name }}
-                    </option>
-                </select>
-            </label>
+        <label class="form-control w-full">
+            <div class="label">
+                <span class="label-text">Tipo de ejercicio</span>
+                <span class="label-text-alt">Crear uno</span>
+            </div>
+            <select class="select select-bordered capitalize">
+                <option disabled selected>
+                    ejercicio
+                </option>
+                <option v-for="ex in store.getExercises">
+                    {{ ex.name }}
+                </option>
+            </select>
+        </label>
 
-            <label class="input input-bordered flex items-center gap-2">
-                <input type="date" class="grow" placeholder="Fecha" v-model="data.date" />
-            </label>
+        <label class="input input-bordered flex items-center gap-2">
+            <input type="date" class="grow" placeholder="Fecha" v-model="data.date" />
+        </label>
 
-            <label class="input input-bordered flex items-center gap-2">
-                <NumberedListIcon class="h-5" />
+        <label class="input input-bordered flex items-center gap-2">
+            <NumberedListIcon class="h-5" />
 
-                <input type="number" class="grow" placeholder="Repeticiones" min="0" v-model="data.reps" />
-            </label>
-            <label class="input input-bordered flex items-center gap-2">
-                <ScaleIcon class="h-5" />
+            <input type="number" class="grow" placeholder="Repeticiones" min="0" v-model="data.reps" />
+        </label>
+        <label class="input input-bordered flex items-center gap-2">
+            <ScaleIcon class="h-5" />
 
-                <input type="number" class="grow" placeholder="Peso" min="0" v-model="data.weight" />
-                <span class="">kg</span>
-            </label>
+            <input type="number" class="grow" placeholder="Peso" min="0" v-model="data.weight" />
+            <span class="">kg</span>
+        </label>
 
-            <button class="btn btn-primary">Submit</button>
-        </div>
+        <button class="btn btn-primary">Submit</button>
     </div>
 </template>
 
@@ -61,11 +59,6 @@ const data = reactive({
 @media (min-width: 1024px) {
     .box {
         min-height: 100vh;
-        max-width: 50%;
-        margin: auto;
-        display: flex;
-        place-items: center;
-        align-items: center;
     }
 }
 </style>
